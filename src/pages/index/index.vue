60853fae6082ee48325e7c89f5075970e5eac4c7
修凤军 <150098837+xiufj@users.noreply.github.com>
2024-06-02 00:18:26 +0800

    feat: 首页

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -8,38 +8,43 @@
 }
 </route>
 <template>
-  <view
-    class="bg-white overflow-hidden pt-2 px-4"
-    :style="{ marginTop: safeAreaInsets?.top + 'px' }"
-  >
-    <view class="mt-12">
-      <image src="/static/logo.svg" alt="" class="w-28 h-28 block mx-auto" />
+  <view class="bg-white overflow-hidden px-2" :style="{ marginTop: safeAreaInsets?.top + 'px' }">
+    <wd-swiper
+      :list="swiperList"
+      autoplay
+      :current="0"
+      @click="handleClick"
+      @change="onChange"
+    ></wd-swiper>
+    <view
+      class="border border-solid border-dark-50 rounded flex justify-center justify-between py-4 px-6"
+    >
+      <view class="flex flex-col gap-2 items-center">
+        <wd-icon name="add-circle" class="text-6xl" />
+        <text>爽约包赔</text>
+      </view>
+      <view class="flex flex-col gap-2 items-center">
+        <wd-icon name="add-circle" class="text-6xl" />
+        <text>爽约包赔</text>
+      </view>
+      <view class="flex flex-col gap-2 items-center">
+        <wd-icon name="add-circle" class="text-6xl" />
+        <text>爽约包赔</text>
+      </view>
+      <view class="flex flex-col gap-2 items-center">
+        <wd-icon name="add-circle" class="text-6xl" />
+        <text>爽约包赔</text>
+      </view>
     </view>
-    <view class="text-center text-4xl main-title-color mt-4 test-unocss-apply">unibest</view>
-    <view class="text-center text-2xl mt-2 mb-8">最好用的 uniapp 开发模板</view>
-    <view class="text-justify max-w-100 m-auto text-4 indent mb-2">{{ description }}</view>
-    <view class="mt-8 text-center">
-      <text class="text-green-400">在线文档：</text>
-      <!-- #ifndef H5 -->
-      https://codercup.github.io/unibest-docs/
-      <!-- #endif -->
-      <!-- #ifdef H5 -->
-      <a href="https://codercup.github.io/unibest-docs/" target="_blank">
-        https://codercup.github.io/unibest-docs/
-      </a>
-      <!-- #endif -->
-    </view>
-    <view class="mt-4 text-center">
-      <text class="text-green-400">在线预览：</text>
-      <!-- #ifndef H5 -->
-      https://codercup.github.io/unibest/
-      <!-- #endif -->
-      <!-- #ifdef H5 -->
-      <a href="https://codercup.github.io/unibest/" target="_blank">
-        https://codercup.github.io/unibest/
-      </a>
-      <!-- #endif -->
-    </view>
+    <wd-segmented :options="list" v-model:value="current1"></wd-segmented>
+
+    <wd-tabs v-model="tab">
+      <block v-for="item in 4" :key="item">
+        <wd-tab :title="`标签${item}`">
+          <view class="content">内容{{ item }}</view>
+        </wd-tab>
+      </block>
+    </wd-tabs>
   </view>
 </template>
 
@@ -47,32 +52,32 @@
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
 
-const author = ref('菲鸽')
-const description = ref(
-  'unibest 是一个集成了多种工具和技术的 uniapp 开发模板，由 uniapp + Vue3 + Ts + Vite4 + UnoCss + UniUI + VSCode 构建，模板具有代码提示、自动格式化、统一配置、代码片段等功能，并内置了许多常用的基本组件和基本功能，让你编写 uniapp 拥有 best 体验。',
-)
+const swiperList = ref([
+  'https://unpkg.com/wot-design-uni-assets/redpanda.jpg',
+  'https://unpkg.com/wot-design-uni-assets/capybara.jpg',
+  'https://unpkg.com/wot-design-uni-assets/panda.jpg',
+  'https://img.yzcdn.cn/vant/cat.jpeg',
+  'https://unpkg.com/wot-design-uni-assets/meng.jpg',
+])
+const aa = reactive({})
+const current1 = ref('李雷')
+const list = ref([
+  {
+    value: '助教',
+  },
+  {
+    value: '优惠券',
+  },
+  {
+    value: '合作球房',
+  },
+])
 
-onLoad(() => {
-  console.log(author)
-})
-
-/** 激活“分享给好友” */
-onShareAppMessage((options: Page.ShareAppMessageOption): Page.CustomShareContent => {
-  console.log('options:', options)
-  return {
-    title: 'unibest',
-    desc: 'unibest 演示示例',
-    path: '/pages/index/index?id=xxx',
-  }
-})
+const tab = ref(0)
+function handleClick(e) {
+  console.log(e)
+}
+function onChange(e) {
+  console.log(e)
+}
 </script>
-
-<style>
-.main-title-color {
-  color: #d14328;
-}
-
-.test-unocss-apply {
-  @apply m-4;
-}
-</style>
